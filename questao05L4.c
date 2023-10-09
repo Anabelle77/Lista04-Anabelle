aa3f23fa08dcbe6966963d1f753d40b74098ada0
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-10-09 19:35:15 -0300

    Update questao05L4.c

--- questao05L4.c
+++ questao05L4.c
@@ -1,35 +1,39 @@
-//5. Um vendedor necessita de um programa que calcule o preço total devido por um cliente. O programa deve receber o código de um produto e a quantidade comprada e calcular o preço total, usando a tabela abaixo. Mostrar uma mensagem no caso de código inválido.
-
+//5. Um vendedor necessita de um programa que calcule o preço total devido por um cliente. 
+//O programa deve receber o código de um produto e a quantidade comprada e calcular o preço total, usando a tabela abaixo.
+//Mostrar uma mensagem no caso de código inválido.
 #include <stdio.h>
-#include <stdbool.h>
-
-float DevolverValor(int code){
-  if(code == 1001) return 5.32;
-  else if(code == 1324) return 6.45;
-  else if(code == 6548) return 2.37;
-  else if(code == 987) return 5.32;
-  else if(code == 7623) return 6.45;
-}
 
 int main() {
-  int code, quantidade;
-  float valor, total = 0;
-    
-  while(true){
-    printf("Digite o codigo do produto ou '0' para finalizar: ");
-    scanf("%i", &code);
-        
-    if(code == 0) break;
-    
-    valor = DevolverValor(code);
-    if(valor == 0) {
-      printf("Codigo inválido, tente novamente.\n");
-      continue;
-    }
-        
-    total += valor;
-    quantidade++;
+  int codigo;
+  int quantidade;
+  float preco_total = 0.0;
+
+  printf("Digite o código do produto: ");
+  scanf("%d", &codigo);
+
+  printf("Digite a quantidade comprada: ");
+  scanf("%d", &quantidade);
+
+  switch (codigo) {
+    case 1001:
+      preco_total = quantidade * 5.32;
+      break;
+    case 1324:
+      preco_total = quantidade * 6.45;
+      break;
+    case 6548:
+      preco_total = quantidade * 2.37;
+      break;
+    case 987:
+      preco_total = quantidade * 5.32;
+      break;
+    case 7623:
+      preco_total = quantidade * 6.45;
+      break;
+    default:
+      printf("Código de produto inválido.\n");
+      return 1;
   }
-  printf("\nNo total a sua de %i produtos custou R$ %.2f.\n", quantidade, total);
+  printf("O preço total é: R$ %.2f\n", preco_total);
   return 0;
 }
