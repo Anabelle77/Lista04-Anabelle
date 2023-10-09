a46c23956b6b0ce020f1f7faa8c14164c5f1f67e
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-10-09 19:21:23 -0300

    Update questao04L4.c

--- questao04L4.c
+++ questao04L4.c
@@ -1,4 +1,6 @@
-//4. Um Banco concederá um crédito especial aos seus clientes, variável com o saldo médio no último ano. Faça um algoritmo que leia o saldo médio de um cliente e calcule o valor do crédito de acordo com a tabela abaixo. Mostre uma mensagem informando o saldo médio e o valor do crédito.
+//4. Um Banco concederá um crédito especial aos seus clientes, variável com o saldo médio no último ano. 
+//Faça um algoritmo que leia o saldo médio de um cliente e calcule o valor do crédito de acordo com a tabela abaixo.
+//Mostre uma mensagem informando o saldo médio e o valor do crédito.
 
 #include <stdio.h>
 
